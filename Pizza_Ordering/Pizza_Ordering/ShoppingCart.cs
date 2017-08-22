a92714b14d90e7a6df90cf03a7bd58667e17b894
ssaraf <ssaraf@tavisca.com>
2017-08-22 14:15:50 +0530

    Problem in returning the selected values from menu

---
+++ Pizza_Ordering/Pizza_Ordering/ShoppingCart.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza_Ordering
+{
+    public class ShoppingCart
+    {
+        private List<string> _pizzaInCart = new List<string>();
+
+
+        public void Add()
+        {
+            Customer customer = new Customer();
+            _pizzaInCart.Add(customer.Select());
+        }
+
+
+        public List<string> View()
+        {
+            return _pizzaInCart;
+        }
+
+
+        public void Order()
+        {
+            if (_pizzaInCart.Count > 2)
+            {
+                var totalAmount = _pizzaInCart
+            }
+
+        }
+    }
+}
